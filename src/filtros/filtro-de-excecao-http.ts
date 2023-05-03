1679594680a795bc77377dce9ecc7816b66c5584
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-05-03 16:12:05 -0300

    adaptando código para express e fastify

--- src/filtros/filtro-de-excecao-http.ts
+++ src/filtros/filtro-de-excecao-http.ts
@@ -6,27 +6,33 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 
-import { Request, Response } from 'express';
+import { HttpAdapterHost } from '@nestjs/core';
 
 @Catch()
 export class FiltroDeExcecaoHttpGlobal implements ExceptionFilter {
+  constructor(private adapterHost: HttpAdapterHost) {}
+
   catch(excecao: Error, host: ArgumentsHost) {
     console.log(excecao);
 
+    const { httpAdapter } = this.adapterHost;
+
     const contexto = host.switchToHttp();
-    const res = contexto.getResponse<Response>();
-    const req = contexto.getRequest<Request>();
+    const resposta = contexto.getResponse();
+    const requisicao = contexto.getRequest();
 
     const status =
       excecao instanceof HttpException
         ? excecao.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    res.status(status).json({
+    const body = {
       statusCode: status,
       message: excecao.message,
       timestamp: new Date().toISOString(),
-      path: req.url,
-    });
+      path: httpAdapter.getRequestUrl(requisicao),
+    };
+
+    httpAdapter.reply(resposta, body, status);
   }
 }
